7629c87922502879c5bfe19836cbf3bad3297a78
Filip Tomanec <tomanec.f@gmail.com>
2025-03-09 19:42:16 +0100

    Fix mobile nav closing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,46 +6,48 @@ import Photo from "@/components/Photo";
 import Stats from "@/components/Stats";
 
 export default function Home() {
-    // const downloadCV = () => {
-    //     const link = document.createElement('a');
-    //     link.href = '/assets/CV_Filip_Tomanec.pdf';
-    //     link.download = 'CV_Filip_Tomanec.pdf';
-    //     link.click();
-    // }
+  // const downloadCV = () => {
+  //     const link = document.createElement('a');
+  //     link.href = '/assets/CV_Filip_Tomanec.pdf';
+  //     link.download = 'CV_Filip_Tomanec.pdf';
+  //     link.click();
+  // }
 
-    return (
-        <section className="h-full">
-            <div className="container mx-auto h-full">
-                <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
-                    {/* text */}
-                    <div className="text-center xl:text-left order-2 xl:order-none">
-                        <span className="text-xl">Software Developer</span>
-                        <h1 className="h1 mb-6">
-                            Ahoj, jsem<br/> <span className="text-accent">Filip Tomanec</span>
-                        </h1>
-                        <p className="max-w-[500px] mb-9 text-white/80">
-                            Jsem Full-Stack vývojář z Ostravy, zaměřený na tvorbu webových a mobilních aplikací.
-                            Mám zkušenosti s vývojem frontendu v TypeScriptu - React/React-Native a backendu v Javě -
-                            Spring Boot. Mám za sebou projekty v různých odvětvích, kde jsem se podílel na vývoji
-                            robustních a efektivních řešení.
-                        </p>
-                        {/* btn and socials */}
-                        <div className="flex flex-col xl:flex-row items-center gap-8">
-                            <div className="mb-8 xl:mb-0">
-                                <Social
-                                    containerStyles="flex gap-6"
-                                    iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
-                                />
-                            </div>
-                        </div>
-                    </div>
-                    {/* photo */}
-                    <div className="order-1 xl:order-none mb-8 xl:mb-0">
-                        <Photo/>
-                    </div>
-                </div>
+  return (
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          {/* text */}
+          <div className="text-center xl:text-left order-2 xl:order-none">
+            <span className="text-xl">Software Developer</span>
+            <h1 className="h1 mb-6">
+              Ahoj, jsem
+              <br /> <span className="text-accent h2">Filip Tomanec</span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              Jsem Full-Stack vývojář z Ostravy, zaměřený na tvorbu webových a
+              mobilních aplikací. Mám zkušenosti s vývojem frontendu v
+              TypeScriptu - React/React-Native a backendu v Javě - Spring Boot.
+              Mám za sebou projekty v různých odvětvích, kde jsem se podílel na
+              vývoji robustních a efektivních řešení.
+            </p>
+            {/* btn and socials */}
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <div className="mb-8 xl:mb-0">
+                <Social
+                  containerStyles="flex gap-6"
+                  iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
+                />
+              </div>
             </div>
-            <Stats/>
-        </section>
-    );
+          </div>
+          {/* photo */}
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+            <Photo />
+          </div>
+        </div>
+      </div>
+      <Stats />
+    </section>
+  );
 }
--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,69 +1,73 @@
 "use client";
 
-import {Sheet, SheetContent, SheetTrigger} from "@/components/ui/sheet";
-import {usePathname} from "next/navigation";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { usePathname } from "next/navigation";
 import Link from "next/link";
-import {CiMenuFries} from "react-icons/ci";
+import { CiMenuFries } from "react-icons/ci";
+import { useState } from "react";
 
 const links = [
-    {
-        name: "domů",
-        path: "/",
-    },
-    {
-        name: "služby",
-        path: "/services",
-    },
-    {
-        name: "životopis",
-        path: "/resume",
-    },
-    // {
-    //     name: "práce",
-    //     path: "/work",
-    // },
-    {
-        name: "kontakt",
-        path: "/contact",
-    },
+  {
+    name: "domů",
+    path: "/",
+  },
+  {
+    name: "služby",
+    path: "/services",
+  },
+  {
+    name: "životopis",
+    path: "/resume",
+  },
+  // {
+  //     name: "práce",
+  //     path: "/work",
+  // },
+  {
+    name: "kontakt",
+    path: "/contact",
+  },
 ];
 
 const MobileNav = () => {
-    const pathname = usePathname();
-    return (
-        <Sheet>
-            <SheetTrigger className="flex justify-center items-center">
-                <CiMenuFries className="text-[32px] text-accent"/>
-            </SheetTrigger>
-            <SheetContent className="flex flex-col">
-                {/* logo */}
-                <div className="mt-32 mb-40 text-center text-2xl">
-                    <Link href="/">
-                        <h1 className="text-4xl font-semibold">
-                            Filip<span className="text-accent">.</span>
-                        </h1>
-                    </Link>
-                </div>
-                {/* nav */}
-                <nav className="flex flex-col justify-center items-center gap-8">
-                    {links.map((link, index) => {
-                        return (
-                            <Link
-                                href={link.path}
-                                key={index}
-                                className={`${
-                                    link.path === pathname &&
-                                    "text-accent border-b-2 border-accent"
-                                } text-xl capitalize hover:text-accent transition-all`}
-                            >
-                                {link.name}
-                            </Link>
-                        );
-                    })}
-                </nav>
-            </SheetContent>
-        </Sheet>
-    );
+  const pathname = usePathname();
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <Sheet open={isOpen} onOpenChange={setIsOpen}>
+      <SheetTrigger className="flex justify-center items-center">
+        <CiMenuFries className="text-[32px] text-accent" />
+      </SheetTrigger>
+      <SheetContent className="flex flex-col">
+        {/* logo */}
+        <div className="mt-32 mb-40 text-center text-2xl">
+          <Link href="/" onClick={() => setIsOpen(false)}>
+            <h1 className="text-4xl font-semibold">
+              Filip<span className="text-accent">.</span>
+            </h1>
+          </Link>
+        </div>
+        {/* nav */}
+        <nav className="flex flex-col justify-center items-center gap-8">
+          {links.map((link, index) => {
+            return (
+              <Link
+                href={link.path}
+                key={index}
+                onClick={() => setIsOpen(false)}
+                className={`${
+                  link.path === pathname &&
+                  "text-accent border-b-2 border-accent"
+                } text-xl capitalize hover:text-accent transition-all`}
+              >
+                {link.name}
+              </Link>
+            );
+          })}
+        </nav>
+      </SheetContent>
+    </Sheet>
+  );
 };
 
 export default MobileNav;
